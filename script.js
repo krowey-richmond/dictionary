10d8bc9c24b6fab6f7a39ce18a17f0a7feab6ead
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2026-09-13 15:07:50 +0000

    update : work o antonym,synonyms section

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const searchInput = document.querySelector(".search-input");
 const searchForm = document.querySelector(".search-wrapper");
+const wordText = document.querySelector(".word");
+const wordPhonetics = document.querySelector(".phonetics");
+const wordOrigin = document.querySelector(".origin");
+const audioBtn = document.querySelector(".audio-btn");
+const meaningSection = document.querySelector(".meanings");
+const antonymsSection = document.querySelector(".antonyms");
+const synonymsSection = document.querySelector(".synonyms");
+const historySection = document.querySelector(".history");
 
 const API_URL = "https://englishdictionaryapi.com/api/v1/words/";
+
+let apiData = null;
+let audio = null;
 
 async function findWord() {
 	const word = searchInput.value.trim();
@@ -15,11 +26,16 @@ async function findWord() {
 	try {
 		const response = await fetch(`${API_URL}${word}`);
 		if (!response.ok) {
+			if (response.status === 404) {
+				alert("Word not found. Please check the spelling and try again");
+				return;
+			}
 			throw new Error(`HTTP error: ${response.status}`);
-			return;
 		}
 
 		const data = await response.json();
+		apiData = data;
+		audio = new Audio(data.pronunciation.audioUrl);
 		console.log(data);
 	} catch (error) {
 		console.error("Error fetching the word:", error);
@@ -49,8 +65,10 @@ function displayFind() {
 		part.senses.forEach((sense) => {
 			const definition = document.createElement("li");
 			definition.classList.add("definition");
+
 			const definitionText = document.createElement("p");
 			definitionText.textContent = sense.definition;
+
 			definition.append(definitionText);
 			if (sense.example) {
 				const example = document.createElement("p");
@@ -71,11 +89,42 @@ function displayFind() {
 		meaningSection.appendChild(details);
 	});
 
-antonymsSection.textContent = "";
--
+	antonymsSection.textContent = "";
+	const antonymsH2 = document.createElement("h2");
+	antonymsH2.textContent = "antonyms";
 
+	antonymsSection.appendChild(antonymsH2);
 
+	apiData.antonyms.slice(0, 5).forEach((antonym) => {
+		const antonymTag = document.createElement("a");
+
+		antonymTag.textContent = antonym;
+		antonymTag.href = `?q=${antonym}`;
+		antonymsSection.appendChild(antonymTag);
+	});
+
+	synonymsSection.textContent = "";
+	const synonymH2 = document.createElement("h2");
+	synonymH2.textContent = "synonyms";
+
+	synonymsSection.appendChild(synonymH2);
+
+	apiData.synonyms.slice(0, 5).forEach((synonym) => {
+		const synonymTag = document.createElement("a");
+
+		synonymTag.textContent = synonym;
+		synonymTag.href = `?q=${synonym}`;
+		synonymsSection.appendChild(synonymTag);
+	});
 }
+
+historySection.textContent = "";
+const historyH2 = document.createElement("h2");
+historyH2.textContent = "history";
+
+historySection.appendChild(historyH2);
+const historyDiv = document.createElement("div");
+historyDiv.classList.add("tags");
 
 audioBtn.addEventListener("click", () => {
 	if (audio) {
@@ -85,6 +134,7 @@ audioBtn.addEventListener("click", () => {
 
 searchForm.addEventListener("submit", async (event) => {
 	event.preventDefault();
+	await findWord();
 
-	findWord();
+	displayFind();
 });
