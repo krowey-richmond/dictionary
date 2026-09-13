a7aa4158a8a5168706d04083f7641396cea3c434
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2026-09-13 00:47:54 +0000

    Add initial HTML structure, CSS styles, and JavaScript functionality for dictionary app

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchInput = document.querySelector(".search-input");
+const searchForm = document.querySelector(".search-wrapper");
+
+const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+async function findWord() {
+	const word = searchInput.value.trim();
+	if (!word) {
+		alert("Please enter a word to search.");
+		return;
+	}
+
+	console.log("Searching for word:", word);
+
+	try {
+		const response = await fetch(`${API_URL}${word}`);
+		if (!response.ok) {
+			throw new Error(`HTTP error: ${response.status}`);
+			return;
+		}
+
+		const data = await response.json();
+		console.log(data)
+	} catch (error) {
+		console.error("Error fetching the word:", error);
+		alert("An error occurred while fetching the word. Please try again later.");
+	}
+}
+
+searchForm.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	findWord();
+});
