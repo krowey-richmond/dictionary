b2a266631d16ea4802f47dee52761b02802e5c59
Krowey Richmond Borquaye <kroweyrichmond2004@gmail.com>
2026-09-13 02:15:45 +0000

    change api

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const searchInput = document.querySelector(".search-input");
 const searchForm = document.querySelector(".search-wrapper");
 
-const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const API_URL = "https://englishdictionaryapi.com/api/v1/words/";
 
 async function findWord() {
 	const word = searchInput.value.trim();
@@ -20,7 +20,7 @@ async function findWord() {
 		}
 
 		const data = await response.json();
-		console.log(data)
+		console.log(data);
 	} catch (error) {
 		console.error("Error fetching the word:", error);
 		alert("An error occurred while fetching the word. Please try again later.");
